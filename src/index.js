c136d21ddcad2873d93087f54ce4c7980e14b991
codessive <jokurio6@gmail.com>
2022-01-20 18:08:28 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Button from 'react-bootstrap/Button';
+/* Icons installed from https://icons.getbootstrap.com/ */
+
+
+class Top extends React.Component {
+    render() {
+      return (
+        <div id="first-section">
+          <div id="logo">
+              <h1>SDG</h1>
+          </div>
+          <div id="main-content">
+              <h1 id="main-heading">Coming Soon</h1>
+              <p id="main-text">SDG Health Education Mobile App aims to encourage people to adopt<br />
+                  and sustain health promoting lifestyles and practices.</p>
+              
+              <div id="buttons">
+                <p id="tiny-text">Coming Soon On</p>
+                <Button type="button" id="button-google" className="btn btn-outline-secondary">
+                <i class="bi bi-google" width="48" height="48"></i>
+                  
+                  <p className="store-text">Google Play</p>
+                </Button>
+
+                <Button type="button" id="button-apple" className="btn btn-outline-secondary">
+                <i class="bi bi-apple" id="apple-icon"></i>
+                
+  
+                  <p className="store-text">App Store</p>
+                </Button>
+              </div>
+              
+          </div>
+      </div>
+      );
+    }
+  }
+
+// ========================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <Top />,
+    document.getElementById('root')
+  );
+  
